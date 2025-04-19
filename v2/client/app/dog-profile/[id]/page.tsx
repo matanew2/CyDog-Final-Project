6a24328c0460fd13bbb8089f8db4f3641c00c5bd
matanew1 <matanew1@gmail.com>
2025-04-19 18:57:11 +0300

    Fix dog profile error

--- v2/client/app/dog-profile/[id]/page.tsx
+++ v2/client/app/dog-profile/[id]/page.tsx
@@ -1,5 +1,10 @@
-import { DogProfilePage } from "@/components/pages/dog-profile-page"
+import { DogProfilePage } from "@/components/pages/dog-profile-page";
 
-export default function DogProfile({ params }: { params: { id: string } }) {
-  return <DogProfilePage id={params.id} />
+export default async function DogProfile({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = await params;
+  return <DogProfilePage id={id} />;
 }
